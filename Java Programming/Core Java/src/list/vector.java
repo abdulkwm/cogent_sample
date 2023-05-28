39b922aa37f264f56a1c8ea5bb9f3c08fbbe5c5c
abdulkerim <abdu0921@hotmail.com>
2023-05-28 15:39:42 -0400

    first commit

---
+++ Java Programming/Core Java/src/list/vector.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.Vector;
+
+public class vector {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(10);
+        vector.add(20);
+        vector.add(30);
+        vector.add(40);
+        vector.add(50);
+        System.out.println(vector);
+    }
+}
